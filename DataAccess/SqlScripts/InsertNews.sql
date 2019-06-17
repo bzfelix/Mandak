593cb496e3c938d56dd5172ad4a123370e83cf3c
Bárándi Zoltán Félix <42773426+bzfelix@users.noreply.github.com>
2019-06-17 20:48:28 +0200

    Update InsertNews.sql

--- DataAccess/SqlScripts/InsertNews.sql
+++ DataAccess/SqlScripts/InsertNews.sql
@@ -3,9 +3,9 @@ insert into articles (
 	Time, 
 	Conductor) 
 values (
-	'Budavári evangélikus templom',
-	'2019.05.25 19:00',
-	'Kertész Péter és dr. Kretz István');
+	'Budavári evangélikus templom', -- Helyszín
+	'2019.05.25 19:00', -- Időpont
+	'Kertész Péter és dr. Kretz István'); -- Karvezető(k)
 	
 set @articleId = LAST_INSERT_ID();
 
@@ -15,8 +15,8 @@ insert into articletexts (
 	Language,
 	ArticleId )
 values (
-	'dr. Balás István hetvenedik születésnapi emlékkoncert',
-	'Kórusunkat 1993-tól huszonöt éven át a karizmatikus egyéniségű, nagy szeretetnek és tiszteletnek örvendett  dr. Balás István vezette 2018-ban sajnálatosan bekövetkezett haláláig. Emlékének a hetvenedik születésnapján hangversennyel tisztelgünk. Szeretettel várunk mindenkit, aki szerette karnagyunkat, hogy vegyen részt a budavári evangélikus templomban 2019. május 25-én, szombaton 19 órakor kezdődő hangversenyünkön.',
-	1, -- hu
-	@articleId
+	'dr. Balás István hetvenedik születésnapi emlékkoncert', -- Esemény címe
+	'Kórusunkat 1993-tól huszonöt éven át a karizmatikus egyéniségű, nagy szeretetnek és tiszteletnek örvendett  dr. Balás István vezette 2018-ban sajnálatosan bekövetkezett haláláig. Emlékének a hetvenedik születésnapján hangversennyel tisztelgünk. Szeretettel várunk mindenkit, aki szerette karnagyunkat, hogy vegyen részt a budavári evangélikus templomban 2019. május 25-én, szombaton 19 órakor kezdődő hangversenyünkön.', -- Leírás
+	1, -- hu. Ne módosítsuk!
+	@articleId -- Ne módosítsuk!
 )
